3502c995a3e1a412e0edd7315eae1831816a53c7
Seth Ketsi <christambassador12@gmail.com>
2024-06-09 23:37:51 +0000

    updated

--- billing_system/bill.c
+++ billing_system/bill.c
@@ -1,2 +1,13 @@
 #include "bill.h"
 
+void generateBillHeader(char name[50], char date[30]){
+    printf("\n\n");
+}
+
+void generateBillBody(char item[30], int quantity, float price){
+
+}
+
+void generateBillFooter(float total){
+
+}
--- billing_system/bill.h
+++ billing_system/bill.h
@@ -1,3 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+struct items{
+    char item[20];
+    float price;
+    int quantity;
+};
+
+struct orders{
+    char customer[50];
+    char date[50];
+    int numOfItems;
+    struct items itm[50];
+};
+
+
+void generateBillHeader(char name[50], char date[30]);
+
+void generateBillBody(char item[30], int quantity, float price);
+
+void generateBillFooter(float total);
